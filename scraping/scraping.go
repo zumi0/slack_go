fb842f1baa30b973191299daa01f8af8f6983176
Yuto NAKAIZUMI <yutonakaizumi@YutonoMacBook-Pro.local>
2019-07-17 13:07:45 +0900

    make dir

---
+++ scraping/scraping.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+  "fmt"
+  "github.com/PuerkitoBio/goquery"
+)
+
+func main() {
+  url := "https://twitter.com/NuinkTSUKUBA"
+  // get method
+  doc, _ := goquery.NewDocument(url)
+  // css selecter
+  selecter_following := "#page-container > div.ProfileCanopy.ProfileCanopy--withNav.js-variableHeightTopBar > div > div.ProfileCanopy-navBar.u-boxShadow > div > div > div.Grid-cell.u-size2of3.u-lg-size3of4 > div > div > ul > li.ProfileNav-item.ProfileNav-item--following > a > span.ProfileNav-value"
+  selecter_followers := "#page-container > div.ProfileCanopy.ProfileCanopy--withNav.js-variableHeightTopBar > div > div.ProfileCanopy-navBar.u-boxShadow > div > div > div.Grid-cell.u-size2of3.u-lg-size3of4 > div > div > ul > li.ProfileNav-item.ProfileNav-item--followers > a > span.ProfileNav-value"
+  // selected texts
+  following := doc.Find(selecter_following).Text()
+  followers := doc.Find(selecter_followers).Text()
+  fmt.Printf("フォロー：%s, フォロワー：%s", following, followers)
+}
